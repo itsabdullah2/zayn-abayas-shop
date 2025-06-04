c65d156f26f8c6fef0a444d8893fa82947fe75a2
itsabdullah2 <abdullahosman3020@gmail.com>
2025-06-04 14:56:11 +0300

    initialize app pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="flex-1">Home</div>;
+};
+
+export default Home;
---
+++ src/pages/Menu.tsx
@@ -0,0 +1,5 @@
+const Menu = () => {
+  return <div className="flex-1">Menu</div>;
+};
+
+export default Menu;
