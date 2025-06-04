96d0ed7963e6247975b5070c58273cf75dd82420
itsabdullah2 <abdullahosman3020@gmail.com>
2025-06-04 15:15:17 +0300

    generate some constant links

---
+++ src/constants/footerLinks.ts
@@ -0,0 +1,56 @@
+type FooterLinksType = {
+  name: string;
+  path: string;
+};
+
+export const privacyFooterLinks: FooterLinksType[] = [
+  {
+    name: "Terms of use",
+    path: "/",
+  },
+  {
+    name: "Privacy policy",
+    path: "/",
+  },
+];
+
+export const servicesFooterLinks: FooterLinksType[] = [
+  {
+    name: "Shop",
+    path: "/categories",
+  },
+  {
+    name: "Other services",
+    path: "/categories",
+  },
+  {
+    name: "Menu",
+    path: "/menu",
+  },
+];
+
+export const aboutUsFooterLinks: FooterLinksType[] = [
+  {
+    name: "Find information",
+    path: "/about",
+  },
+  {
+    name: "About Us",
+    path: "/about",
+  },
+  {
+    name: "Out vision",
+    path: "/about",
+  },
+];
+
+export const informationFooterLinks: FooterLinksType[] = [
+  {
+    name: "All Categories",
+    path: "/categories",
+  },
+  {
+    name: "Products & Prices",
+    path: "/categories",
+  },
+];
