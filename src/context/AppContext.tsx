ac08fa35fd7caeba3de4bbb1fff34c60500fd023
itsabdullah2 <abdullahosman3020@gmail.com>
2025-06-04 15:19:53 +0300

    initialize the main context for the app

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -4,12 +4,15 @@ import { createContext } from "use-context-selector";
 type AppContextType = {
   theme: string;
   handleTheme: () => void;
+  isNavMenu: boolean;
+  setIsNavMenu: React.Dispatch<React.SetStateAction<boolean>> | undefined;
 };
 
 export const AppContext = createContext<AppContextType | undefined>(undefined);
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [theme, setTheme] = useState<string>("light");
+  const [isNavMenu, setIsNavMenu] = useState<boolean>(false);
 
   const handleTheme = () => {
     setTheme((prev) => (prev === "light" ? "dark" : "light"));
@@ -18,6 +21,8 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const value = {
     theme,
     handleTheme,
+    isNavMenu,
+    setIsNavMenu,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
