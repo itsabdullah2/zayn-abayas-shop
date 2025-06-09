8715f2c90dafb730371d787e2260072b96e3f0c0
itsabdullah2 <abdullahosman3020@gmail.com>
2025-06-09 23:33:37 +0300

    generate new state for 'search popup' and functions to handle open & close of search popup

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createContext } from "use-context-selector";
 
 type AppContextType = {
@@ -6,6 +6,9 @@ type AppContextType = {
   handleTheme: () => void;
   isNavMenu: boolean;
   setIsNavMenu: React.Dispatch<React.SetStateAction<boolean>> | undefined;
+  searchPopup: boolean;
+  handleCloseSearchPopup: () => void;
+  handleOpenSearchPopup: () => void;
 };
 
 export const AppContext = createContext<AppContextType | undefined>(undefined);
@@ -13,16 +16,43 @@ export const AppContext = createContext<AppContextType | undefined>(undefined);
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [theme, setTheme] = useState<string>("light");
   const [isNavMenu, setIsNavMenu] = useState<boolean>(false);
+  const [searchPopup, setSearchPopup] = useState<boolean>(false);
 
   const handleTheme = () => {
     setTheme((prev) => (prev === "light" ? "dark" : "light"));
   };
 
+  const handleCloseSearchPopup = () => {
+    setSearchPopup(false);
+  };
+  const handleOpenSearchPopup = () => {
+    setSearchPopup(true);
+  };
+
+  useEffect(() => {
+    const handleCloseOnPressEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        handleCloseSearchPopup();
+      }
+    };
+
+    document.addEventListener("keydown", handleCloseOnPressEsc);
+
+    return () => {
+      document.removeEventListener("keydown", handleCloseOnPressEsc);
+    };
+  });
+
   const value = {
+    // States
     theme,
-    handleTheme,
     isNavMenu,
+    searchPopup,
+    // Functions
+    handleTheme,
     setIsNavMenu,
+    handleCloseSearchPopup,
+    handleOpenSearchPopup,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
