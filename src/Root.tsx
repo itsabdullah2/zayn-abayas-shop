3f6a9b32cb42d58278f0012592ccfccb79b10440
itsabdullah2 <abdullahosman3020@gmail.com>
2025-06-09 23:35:22 +0300

    render 'search popup'

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import { lazy } from "react";
 import Navbar from "./components/common/navbar/Navbar";
 import Footer from "./components/common/footer/Footer";
+import { useContextSelector } from "use-context-selector";
+import { AppContext } from "./context/AppContext";
 
 const HomePage = lazy(() => import("./pages/Home"));
 const AboutPage = lazy(() => import("./pages/About"));
@@ -10,11 +12,20 @@ const AllCategoriesPage = lazy(() => import("./pages/AllCategories"));
 const CheckoutPage = lazy(() => import("./pages/Checkout"));
 const OrderStatusPage = lazy(() => import("./pages/OrderStatus"));
 const CartPage = lazy(() => import("./pages/Cart"));
+const SearchPopup = lazy(
+  () => import("./components/common/navbar/SearchPopup")
+);
 
 const Root = () => {
+  const searchPopup = useContextSelector(
+    AppContext,
+    (value) => value?.searchPopup
+  );
+
   return (
     <div className="flex flex-col min-h-dvh">
       <Navbar />
+      {searchPopup && <SearchPopup />}
       <Routes>
         <Route path={"/"} element={<HomePage />} />
         <Route path={"/about"} element={<AboutPage />} />
