1ad466e300eb50c3835b545bffcc176f5c659668
itsabdullah2 <abdullahosman3020@gmail.com>
2025-06-18 22:56:26 +0300

    render Hero & FeatureProducts components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,17 @@
+import { lazy } from "react";
+
+const FeaturedProducts = lazy(
+  () => import("@/components/layout/home/FeaturedProducts")
+);
+const Hero = lazy(() => import("@/components/layout/home/Hero"));
+
 const Home = () => {
-  return <div className="flex-1">Home</div>;
+  return (
+    <div className="flex-1 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-10">
+      <Hero />
+      <FeaturedProducts />
+    </div>
+  );
 };
 
 export default Home;
