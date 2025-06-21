b060061636b57cb4d3bab5dfc910c8b6ccdd5785
itsabdullah2 <abdullahosman3020@gmail.com>
2025-06-21 16:13:05 +0300

    change the imported variable name

--- src/components/common/navbar/DeskNavigation.tsx
+++ src/components/common/navbar/DeskNavigation.tsx
@@ -1,17 +1,17 @@
 import { Link, useLocation } from "react-router-dom";
-import { navLinks } from "@/constants/navLinks";
+import { NAV_LINKS } from "@/constants";
 import { FaSearch, FaUser } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import CartDropdown from "./CartDropdown";
 import { useContextSelector } from "use-context-selector";
 import { AppContext } from "@/context/AppContext";
-import {Button} from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 const DeskNavigation = () => {
   const location = useLocation();
   const pathname = location.pathname;
   const [activeLink, setActiveLink] = useState<string>(pathname);
-  const [changeLang, setChangeLang] = useState<string>("en")
+  const [changeLang, setChangeLang] = useState<string>("en");
   const handleOpenSearchPopup = useContextSelector(
     AppContext,
     (ctx) => ctx?.handleOpenSearchPopup
@@ -21,14 +21,14 @@ const DeskNavigation = () => {
     setActiveLink(pathname);
   }, [pathname]);
 
-const handleLanguageToggle = () => {
-  setChangeLang((prev) => prev === "en" ? "ar" : "en")
-}
+  const handleLanguageToggle = () => {
+    setChangeLang((prev) => (prev === "en" ? "ar" : "en"));
+  };
 
   return (
     <>
       <ul className="hidden md:flex items-center gap-5 text-neutral">
-        {navLinks.map((link) => (
+        {NAV_LINKS.map((link) => (
           <li
             key={link.name}
             className={`text-[1.125rem] ${
@@ -43,8 +43,13 @@ const handleLanguageToggle = () => {
       </ul>
       {/* Icons */}
       <div className="hidden md:flex items-center gap-5">
-        <Button className={`text-[17px] text-neutral border border-gray hover:border-light-gray duration-200 ${changeLang === "en" ? "font-noto-kufi" : ""} cursor-pointer`} onClick={handleLanguageToggle}>
-          {changeLang === 'en' ? 'العربية' : 'English'}
+        <Button
+          className={`text-[17px] text-neutral border border-gray hover:border-light-gray duration-200 ${
+            changeLang === "en" ? "font-noto-kufi" : ""
+          } cursor-pointer`}
+          onClick={handleLanguageToggle}
+        >
+          {changeLang === "en" ? "العربية" : "English"}
         </Button>
 
         <button
