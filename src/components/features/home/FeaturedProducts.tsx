a5fe8a476e1e9915834463a9b5d1a8d0612f8ebb
itsabdullah2 <abdullahosman3020@gmail.com>
2025-06-22 21:34:43 +0300

    use try-catch block for error handling & render the updated UI

--- src/components/features/home/FeaturedProducts.tsx
+++ src/components/features/home/FeaturedProducts.tsx
@@ -5,35 +5,62 @@ import { PriceFormatter } from "@/utils/formatePrice";
 import { useEffect, useState } from "react";
 
 const FeaturedProducts = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
   const [data, setData] = useState<ProductType[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const products = await getProducts({
-        eqCol: "category_id",
-        eqVal: CATEGORY_IDS.classic,
-      });
-      setData(products);
+      try {
+        const products = await getProducts({
+          eqCol: "category_id",
+          eqVal: CATEGORY_IDS.classic,
+        });
+        setData(products);
+      } catch (err) {
+        setError("Failed to load featured products, please try again");
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchData();
   }, []);
 
   return (
-    <div className="flex-1 grid grid-cols sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols- gap-5 container">
-      {data.map((item) => (
-        <div key={item.id} className="">
-          <img
-            src={item.product_img}
-            alt={item.product_name}
-            className="w-full sm:w-[400px]"
-          />
-          <p>{item.product_name}</p>
-          <p>{item.product_desc}</p>
-          <p>{PriceFormatter(item.product_price, "en-US")}</p>
-        </div>
-      ))}
-    </div>
+    <section>
+      <h2 className="text-primary font-bold text-3xl mb-5">
+        Featured Products
+      </h2>
+      {loading && <p className="text-center text-text">Loading Products...</p>}
+      {error && <p className="text-red-500">{error}</p>}
+      <div className="flex-1 responsive-grid">
+        {data.map((item) => (
+          <figure key={item.id} className="card-style group">
+            <img
+              src={item.product_img}
+              alt={item.product_name}
+              className="w-full sm:w-[400px]"
+            />
+            <figcaption className="flex justify-between items-center py-4 px-2">
+              <p className="card-title">
+                {item.product_name.length > 25
+                  ? item.product_name.slice(0, 25) + "..."
+                  : item.product_name}
+              </p>
+              <span className="font-medium text-accentA">
+                {PriceFormatter(item.product_price, "en-EG")} L.E
+              </span>
+            </figcaption>
+
+            <div className="action-btns group-hover:top-2">
+              <button className="btn">D</button>
+              <button className="btn">C</button>
+            </div>
+          </figure>
+        ))}
+      </div>
+    </section>
   );
 };
 
