a66274ce7297eab735df47dee6d304f27bc864bc
itsabdullah2 <abdullahosman3020@gmail.com>
2025-06-24 10:47:03 +0300

    component generated to render the user icon & the dropdown of related links

---
+++ src/components/common/navbar/UserDropdown.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useRef, useState } from "react";
+import { FaUser, FaShoppingCart, FaHeart } from "react-icons/fa";
+import { IoIosHelpCircle } from "react-icons/io";
+import { FiLogOut } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+const UserDropdown = () => {
+  const [dropdown, setDropdown] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+  const handleToggleUserDropdown = () => setDropdown((prev) => !prev);
+
+  useEffect(() => {
+    const closeOnClickOutside = (event: MouseEvent) => {
+      const target = event.target;
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(target as Node)
+      ) {
+        setDropdown(false);
+      }
+    };
+
+    document.addEventListener("mousedown", closeOnClickOutside);
+    return () => document.removeEventListener("mousedown", closeOnClickOutside);
+  }, []);
+
+  return (
+    <div className="relative" ref={dropdownRef}>
+      <button
+        role="button"
+        className="cursor-pointer text-primary md:text-neutral hover:text-secondary duration-200"
+        onClick={handleToggleUserDropdown}
+      >
+        <FaUser size={20} />
+      </button>
+
+      {dropdown && (
+        <div className="absolute right-0 top-11 rounded-xl bg-neutral w-50 z-50 py-2 px-2 shadow-lg flex flex-col gap-2">
+          <div className="border-b border-gray-300 pb-2 px-2">
+            <p className="font-semibold text-primary">Hello, Abdullah</p>
+            <p className="text-sm text-text">abdullah@example.com</p>
+          </div>
+
+          <ul className="flex flex-col">
+            <MenuItem
+              to="/orders"
+              icon={<FaShoppingCart size={16} />}
+              label="My Orders"
+            />
+            <MenuItem
+              to="/wishlist"
+              icon={<FaHeart size={16} />}
+              label="Wishlist"
+            />
+            <MenuItem
+              to="/help"
+              icon={<IoIosHelpCircle size={16} />}
+              label="Help & Support"
+            />
+          </ul>
+
+          <div className="border-t border-gray-300 pt-2">
+            <button className="flex items-center gap-1 text-red-600 w-full text-left py-2 cursor-pointer rounded-md hover:text-red-50 px-2 hover:bg-red-900 duration-150">
+              <FiLogOut size={16} /> Logout
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserDropdown;
+
+const MenuItem = ({
+  icon,
+  label,
+  to,
+}: {
+  icon: React.ReactNode;
+  label: string;
+  to: string;
+}) => {
+  return (
+    <li className="text-sm text-gray hover:text-primary px-2 py-2 hover:bg-light-gray rounded-md w-full duration-150">
+      <Link to={to} className="flex items-center gap-1 cursor-pointer">
+        {icon} {label}
+      </Link>
+    </li>
+  );
+};
