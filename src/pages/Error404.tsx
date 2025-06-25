8442c396f63a45946e9ad014d76c824073852b22
itsabdullah2 <abdullahosman3020@gmail.com>
2025-06-26 01:41:45 +0300

    initialize the component

---
+++ src/pages/Error404.tsx
@@ -0,0 +1,5 @@
+const Error404 = () => {
+  return <div className="flex-1">Error404</div>;
+};
+
+export default Error404;
---
+++ src/pages/Orders.tsx
@@ -0,0 +1,5 @@
+const Orders = () => {
+  return <div className="flex-1">Orders</div>;
+};
+
+export default Orders;
