b448439f4bfa0e459a7a34b905901529f6ed4111
itsabdullah2 <abdullahosman3020@gmail.com>
2025-06-25 17:38:33 +0300

    render the cart item

---
+++ src/components/common/navbar/CartItem.tsx
@@ -0,0 +1,25 @@
+import type { ProductType } from "@/types";
+import { FaTrashAlt } from "react-icons/fa";
+
+const CartItem = ({ item }: { item: ProductType }) => {
+  return (
+    <div className="flex items-center gap-4">
+      <img
+        src={item.product_img}
+        alt={item.product_name}
+        className="w-16 h-16 object-cover rounded-md"
+      />
+      <div className="flex-1">
+        <h3 className="font-medium text-primary text-[0.9rem]">
+          {item.product_name}
+        </h3>
+        <p className="text-sm text-text">{item.product_price.toFixed(2)} E.L</p>
+      </div>
+      <button className="text-text hover:text-red-700 duration-200 cursor-pointer">
+        <FaTrashAlt size={16} />
+      </button>
+    </div>
+  );
+};
+
+export default CartItem;
