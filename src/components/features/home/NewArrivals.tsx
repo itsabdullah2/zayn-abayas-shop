c5a0d2f64a0e79488da20a8a85a2ddddee746156
itsabdullah2 <abdullahosman3020@gmail.com>
2025-06-28 21:18:37 +0300

    fix responsiveness issue

--- src/components/features/home/NewArrivals.tsx
+++ src/components/features/home/NewArrivals.tsx
@@ -49,7 +49,7 @@ const NewArrivals = () => {
               src={item.product_img}
               alt={item.product_name}
               loading="lazy"
-              className="w-full sm:w-[400px]"
+              className="w-full"
             />
             <figcaption className="flex justify-between items-center py-4 px-2">
               <p className="card-title">
