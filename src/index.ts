5f93fd423a9233d13045a7aaa423e92650c78727
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-13 06:14:19 +0300

    export pages from one place

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 // Export all components from one place
 
-// Export Pages
-export { default as HomePage } from "./pages/Home";
-export { default as AboutPage } from "./pages/About";
-export { default as ShopPage } from "./pages/Shop";
-export { default as AllCategoriesPage } from "./pages/AllCategories";
-export { default as CheckoutPage } from "./pages/Checkout";
-export { default as OrderStatusPage } from "./pages/OrderStatus";
-export { default as CartPage } from "./pages/Cart";
+import { lazy } from "react";
+
+export { default as Navbar } from "./components/common/navbar/Navbar";
+export { default as Footer } from "./components/common/footer/Footer";
+
+export const HomePage = lazy(() => import("./pages/Home"));
+export const AboutPage = lazy(() => import("./pages/About"));
+export const ShopPage = lazy(() => import("./pages/Shop"));
+export const AllCategoriesPage = lazy(() => import("./pages/AllCategories"));
+export const CheckoutPage = lazy(() => import("./pages/Checkout"));
+export const OrderStatusPage = lazy(() => import("./pages/OrderStatus"));
+export const CartPage = lazy(() => import("./pages/Cart"));
+export const SearchPopup = lazy(
+  () => import("./components/common/search/SearchPopup")
+);
+export const ProductDetailsPopup = lazy(
+  () => import("./components/common/details_popup")
+);
+export const OrdersPage = lazy(() => import("./pages/Orders"));
+export const Error404Page = lazy(() => import("./pages/Error404"));
+export const SignInPage = lazy(() => import("./pages/SignIn"));
+export const SignUpPage = lazy(() => import("./pages/SignUp"));
+export const ProductDetailsPage = lazy(() => import("./pages/ProductDetails"));
