3fb79fd5837ddbe58cc3e5e41d5d744efb6600bf
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-13 06:11:18 +0300

    render product details page

---
+++ src/pages/ProductDetails.tsx
@@ -0,0 +1,11 @@
+import ProductDetails from "@/components/features/product-details";
+
+const ProductDetailsPage = () => {
+  return (
+    <section className="flex-1 bg-neutral">
+      <ProductDetails />
+    </section>
+  );
+};
+
+export default ProductDetailsPage;
