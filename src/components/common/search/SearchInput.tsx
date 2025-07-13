fd0a4a13a3d2563ca71332698cd925c6a972346e
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-13 06:15:43 +0300

    finish the search functionality

--- src/components/common/search/SearchInput.tsx
+++ src/components/common/search/SearchInput.tsx
@@ -1,40 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef } from "react";
 import { Input } from "@/components/ui/input";
-import useDebounce from "@/hooks/useDebounce";
 
-interface Props {
-  onDebounceSearch?: (searchTerm: string) => string;
-}
+type SearchHookType = {
+  searchQuery: string;
+  setSearchQuery: (value: string) => void;
+  searchProducts: (params: { query: string }) => void;
+  loading?: boolean;
+};
 
-const SearchInput = ({ onDebounceSearch }: Props) => {
-  const [inputValue, setInputValue] = useState<string>("");
-  const debouncedSearchTerm = useDebounce(inputValue, 500);
+const SearchInput = ({
+  searchQuery,
+  setSearchQuery,
+  searchProducts,
+  loading,
+}: SearchHookType) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setInputValue(value);
+    setSearchQuery(value);
   };
 
-  useEffect(() => {
-    // Only trigger the callback if it's provided & the debounced term isn't null/undefined
-    if (
-      onDebounceSearch &&
-      debouncedSearchTerm !== undefined &&
-      debouncedSearchTerm !== null
-    ) {
-      onDebounceSearch(debouncedSearchTerm);
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      searchProducts({ query: searchQuery });
     }
-  }, [onDebounceSearch, debouncedSearchTerm]);
+  };
 
   return (
     <Input
+      ref={inputRef}
       type="search"
-      value={inputValue}
+      value={searchQuery}
       onChange={handleChange}
+      onKeyDown={handleKeyDown}
+      disabled={loading}
       placeholder="Search..."
       className="w-full border border-gray rounded-lg h-11 bg-light-gray focus:placeholder:opacity-0 placeholder:duration-200 focus:outline-none mt-5 focus-visible:ring-none focus:border-none"
+      aria-label="Search products"
+      autoFocus
     />
   );
 };
 
-export default SearchInput;
+export default React.memo(SearchInput);
--- src/components/common/search/SearchPopup.tsx
+++ src/components/common/search/SearchPopup.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { AppContext } from "@/context/AppContext";
-import { Link } from "react-router-dom";
 import { useContextSelector } from "use-context-selector";
 import SearchInput from "./SearchInput";
 import { useEffect, useRef } from "react";
+import SearchResults from "./SearchResults";
+import useSearch from "@/hooks/useSearch";
 
 const SearchPopup = () => {
+  const searchHook = useSearch();
   const popupRef = useRef<HTMLDivElement | null>(null);
 
   const handleCloseSearchPopup = useContextSelector(
@@ -30,26 +32,28 @@ const SearchPopup = () => {
 
   return (
     <>
-      <div className="absolute bg-black/70 top-0 left-0 w-full h-full z-90" />
+      <div className="fixed bg-black/70 top-0 left-0 w-full h-full z-90" />
       <div
-        className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-100 bg-neutral text-primary py-8 px-5 rounded-xl w-[95vw] sm:w-[40.625rem] h-[28.125rem] max-h-[37.5rem] overflow-y-auto `}
+        className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-100 bg-neutral text-primary py-8 px-5 rounded-xl w-[95vw] sm:w-[40.625rem] h-[28.125rem] max-h-[37.5rem] overflow-y-auto `}
         ref={popupRef}
+        role="dialog"
+        aria-modal={true}
       >
         <div className="flex flex-col gap-5">
-          <SearchInput />
-
-          <ul className="flex flex-col gap-3">
-            <li className="odd:bg-light-gray/50 even:bg-light-gray py-4 px-2 rounded-lg h-fit">
-              <Link to="#" className="flex flex-col">
-                <h3 className="font-medium text-black">First Result</h3>
-                <p className="text-gray text-sm">
-                  {/* Condition here to handle the number of characters if the length of the desc > 80 character will render 80 characters with => ... */}
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Corporis, maiores!
-                </p>
-              </Link>
-            </li>
-          </ul>
+          <SearchInput
+            searchQuery={searchHook.searchQuery}
+            setSearchQuery={searchHook.setSearchQuery}
+            searchProducts={searchHook.searchProducts}
+            loading={searchHook.loading}
+          />
+          <SearchResults
+            products={searchHook.products}
+            loading={searchHook.loading}
+            error={searchHook.error}
+            searchQuery={searchHook.searchQuery}
+            hasSearched={!!searchHook.hasSearched}
+            onResultClick={handleCloseSearchPopup}
+          />
         </div>
 
         <Button
@@ -57,6 +61,7 @@ const SearchPopup = () => {
           size="sm"
           className="bg-gray/30 text-primary border border-text absolute top-2 right-5 cursor-pointer hover:bg-light-gray duration-200"
           onClick={handleCloseSearchPopup}
+          aria-label="Close search popup"
         >
           Esc
         </Button>
