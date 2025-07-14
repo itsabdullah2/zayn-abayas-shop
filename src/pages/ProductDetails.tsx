bd526a241cb249505adca830a3d13e204676a5bc
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-14 22:13:59 +0300

    remove section tag

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -2,9 +2,9 @@ import ProductDetails from "@/components/features/product-details";
 
 const ProductDetailsPage = () => {
   return (
-    <section className="flex-1 bg-neutral">
+    <>
       <ProductDetails />
-    </section>
+    </>
   );
 };
 
