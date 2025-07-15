c4ebba25f928b7e04cc28b7fe6db6f40661d99a2
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-15 20:52:22 +0300

    sperate the UI into components

--- src/components/features/product-details/index.tsx
+++ src/components/features/product-details/index.tsx
@@ -1,20 +1,18 @@
 import Loading from "@/components/layout/Loading";
-import useCartData from "@/hooks/useCartData";
 import { supabase } from "@/supabase";
 import type { ProductType } from "@/types";
 import { PriceFormatter } from "@/utils/formatePrice";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { FaStar } from "react-icons/fa";
+import CartBtns from "./CartBtns";
+import QuantityBtns from "./QuantityBtns";
+import ColorSelector from "./ColorSelector";
+import Stars from "./Stars";
 
 const ProductDetails = () => {
   const { id } = useParams();
   const [product, setProduct] = useState<ProductType | null>(null);
   const [loading, setLoading] = useState(false);
-  const { handleCart } = useCartData();
-  const [hover, setHover] = useState(0);
-  const [rating, setRating] = useState(0);
-  const [color, setColor] = useState("white");
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -64,68 +62,24 @@ const ProductDetails = () => {
         </div>
 
         <div className="lg:col-span-1 flex flex-col gap-5">
-          <div className="flex flex-col gap-3">
-            <h4 className="text-2xl text-primary">{product.product_name}</h4>
-            <div className="flex items-center gap-1">
-              {Array.from({ length: 5 }).map((_, i) => {
-                const star = i + 1;
-                return (
-                  <FaStar
-                    key={i}
-                    onMouseEnter={() => setHover(star)}
-                    onMouseLeave={() => setHover(0)}
-                    onClick={() => {
-                      setRating(star);
-                      // Send to the backend
-                    }}
-                    className={`${
-                      star <= (hover || rating)
-                        ? "text-yellow-400"
-                        : "text-gray-400"
-                    } cursor-pointer duration-200`}
-                  />
-                );
-              })}
-            </div>
+          <h4 className="text-2xl text-primary font-">
+            {product.product_name}
+          </h4>
 
-            <div className="flex flex-col mt-2">
-              <h4 className="font-medium text-primary text-3xl">
-                {PriceFormatter(product.product_price, "en-EG")} L.E
-              </h4>
+          <Stars />
 
-              <div className="flex items-center gap-1 mt-4">
-                <button
-                  className="h-4 w-4 border border-primary rounded-full p-[2px] cursor-pointer"
-                  onClick={() => setColor("white")}
-                >
-                  <span className="bg-primary h-full w-full block rounded-full" />
-                </button>
-                <button
-                  className="h-4 w-4 border border-primary rounded-full p-[2px] cursor-pointer"
-                  onClick={() => setColor("black")}
-                >
-                  <span className="bg-primary h-full w-full block rounded-full" />
-                </button>
-              </div>
-            </div>
-            <p className="text-text text-[15px]">{product.product_desc}</p>
+          <div className="flex flex-col mt-2">
+            <h4 className="font-medium text-primary text-3xl">
+              {PriceFormatter(product.product_price, "en-EG")} L.E
+            </h4>
 
-            <div className="flex items-center justify-between gap-2">
-              <div className="flex items-center gap-2">
-                <button
-                  className="primary-btn px-3! h-auto! py-1 text-[15px] relative overflow-hidden group cursor-pointer"
-                  onClick={() => handleCart?.(product.id)}
-                >
-                  Add to cart
-                  <span className="shine-effect group-hover:animate-shine" />
-                </button>
-                <button className="primary-btn px-3! h-auto! py-1 text-[15px] relative overflow-hidden group cursor-pointer">
-                  Add to wishlist
-                  <span className="shine-effect group-hover:animate-shine" />
-                </button>
-              </div>
-            </div>
+            <ColorSelector />
           </div>
+
+          <QuantityBtns productId={product.id} />
+          <CartBtns productId={product.id} />
+
+          <p className="text-text text-[15px]">{product.product_desc}</p>
         </div>
       </div>
     </section>
