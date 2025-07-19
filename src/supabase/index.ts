3ddcd3757da32053861272df70a323776914dd68
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-19 12:48:22 +0300

    export reviews functions

--- src/supabase/index.ts
+++ src/supabase/index.ts
@@ -5,10 +5,17 @@ import { signUpWithEmail } from "./auth/signUp";
 import { signOut } from "./auth/signOut";
 import { signInWithOAuth } from "./auth/OAuth";
 
+import { getReviews } from "./db/reviews";
+import { addReview } from "./db/reviews";
+import { updateReview } from "./db/reviews";
+
 export {
   supabase,
   signInWithPassword,
   signUpWithEmail,
   signInWithOAuth,
   signOut,
+  getReviews,
+  addReview,
+  updateReview,
 };
