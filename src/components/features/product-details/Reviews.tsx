06c258db424a5de2ea01ab82c2ed7c93582583e3
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-19 06:30:09 +0300

    pass the values of PlacedReviews props

--- src/components/features/product-details/Reviews.tsx
+++ src/components/features/product-details/Reviews.tsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import PlacedReviews from "./PlacedReviews";
 import AddReview from "./AddReview";
+import { useContextSelector } from "use-context-selector";
+import { AuthContext } from "@/context/AuthContext";
 
 const Reviews = () => {
   const [isReviews, setIsReviews] = useState("reviews");
+  const user = useContextSelector(AuthContext, (ctx) => ctx?.user);
+
+  const reviews: string[] = []; // Just a Placeholder til connecting to DB
 
   let content;
   if (isReviews === "reviews") {
-    content = <PlacedReviews />;
+    const username = user?.user_metadata?.username;
+    content = <PlacedReviews reviews={reviews} username={username} />;
   } else if (isReviews === "add-review") {
     content = <AddReview />;
   }
 
   return (
     <div className="flex flex-col gap-5">
-      <div className="flex items-center pb-2 border-b border-gray-400 gap-4">
+      <div className="flex items-center pb-2 border-b border-soft-gray gap-4">
         <button
           className={`text-[15px] cursor-pointer ${
             isReviews === "reviews" ? "text-primary font-medium" : "text-text "
