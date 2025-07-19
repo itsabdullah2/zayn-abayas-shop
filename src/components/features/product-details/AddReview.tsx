44816411ca8da3fe1f577a453a8abc834601fd20
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-19 12:44:25 +0300

    add review to the db functionality

--- src/components/features/product-details/AddReview.tsx
+++ src/components/features/product-details/AddReview.tsx
@@ -3,20 +3,33 @@ import Stars from "./Stars";
 import { useContextSelector } from "use-context-selector";
 import { AuthContext } from "@/context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { addReview } from "@/supabase";
 
-const AddReview = () => {
-  const [reviewVal, setReviewVal] = useState("");
+type Props = {
+  productId: string;
+  refreshReviews: () => void;
+};
+
+const AddReview = ({ productId, refreshReviews }: Props) => {
   const [error, setError] = useState("");
-  const isAuthenticated = useContextSelector(
-    AuthContext,
-    (ctx) => ctx?.isAuthenticated
-  );
+  const [reviewData, setReviewData] = useState({
+    reviewVal: "",
+    rating: 0,
+  });
+
+  const { isAuthenticated, user } = useContextSelector(AuthContext, (ctx) => ({
+    isAuthenticated: ctx?.isAuthenticated,
+    user: ctx?.user,
+  }));
   const navigate = useNavigate();
 
   const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setReviewVal(e.target.value);
+    setReviewData((prev) => ({
+      ...prev,
+      reviewVal: e.target.value,
+    }));
   };
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!isAuthenticated) {
@@ -24,13 +37,32 @@ const AddReview = () => {
       return;
     }
 
-    if (reviewVal.trim() === "") {
+    if (reviewData.reviewVal.trim() === "") {
       setError("Please add a review!");
       return;
     }
-    setError("");
+
     // save in DB - later
-    setReviewVal("");
+    if (user && isAuthenticated) {
+      const data = {
+        product_id: productId,
+        user_id: user.id,
+        comment: reviewData.reviewVal,
+        rating: reviewData.rating,
+      };
+      await addReview(data);
+      refreshReviews();
+    }
+    setError("");
+    setReviewData({ reviewVal: "", rating: 0 });
+  };
+
+  const handleRating = (star: number) => {
+    setReviewData((prev) => ({
+      ...prev,
+      rating: star,
+    }));
+    // DB Logic goes here
   };
 
   return (
@@ -40,7 +72,7 @@ const AddReview = () => {
           placeholder="Add a review..."
           maxLength={600}
           className="bg-light-gray h-[180px] resize-none py-2 px-4 focus:outline-none focus:placeholder:opacity-0 placeholder:duration-200 caret-accentA text-primary text-[15px]"
-          value={reviewVal}
+          value={reviewData.reviewVal}
           onChange={handleTextareaChange}
         />
 
@@ -55,7 +87,11 @@ const AddReview = () => {
           <span className="shine-effect group-hover:animate-shine" />
         </button>
       </form>
-      <Stars className="md:basis-58 text-3xl" />
+      <Stars
+        className="md:basis-58 text-3xl"
+        rating={reviewData.rating}
+        onClick={handleRating}
+      />
     </div>
   );
 };
