12f9df065fb4375a55b8195f7b278471602b1aeb
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-19 12:48:59 +0300

    generate reviews, review likes, and user table types

--- src/supabase/types/index.ts
+++ src/supabase/types/index.ts
@@ -10,3 +10,27 @@ export type CategoriesTableType = {
   category: string;
   created_at: Date;
 };
+
+export type UserTableType = {
+  id: string;
+  username: string;
+  email: string;
+  created_at: string;
+};
+
+export type ReviewsTableType = {
+  id?: string;
+  product_id: string;
+  user_id: string;
+  comment: string;
+  rating: number;
+  create_at?: Date;
+};
+
+export type ReviewLikesTableType = {
+  id?: string;
+  user_id: string;
+  review_id: string;
+  likes: number;
+  created_at?: Date;
+};
