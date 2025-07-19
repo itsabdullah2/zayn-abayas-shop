f32dd982506d42479a051bf1e42f36e305f07ab0
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-19 06:24:58 +0300

    change color code to variable & memoize the component props

--- src/components/features/product-details/CartBtns.tsx
+++ src/components/features/product-details/CartBtns.tsx
@@ -1,9 +1,10 @@
 import useCartData from "@/hooks/useCartData";
+import React from "react";
 
 const CartBtns = ({ productId }: { productId: string }) => {
   const { handleCart } = useCartData();
   return (
-    <div className="flex items-center gap-3 w-full pb-5 border-b border-gray-400">
+    <div className="flex items-center gap-3 w-full pb-5 border-b border-soft-gray">
       <button
         className="flex-1 border border-primary text-primary font-medium rounded-none px-3 h-auto py-1 text-[15px] hover:bg-primary hover:text-neutral duration-200 cursor-pointer"
         onClick={() => handleCart?.(productId)}
@@ -18,4 +19,4 @@ const CartBtns = ({ productId }: { productId: string }) => {
   );
 };
 
-export default CartBtns;
+export default React.memo(CartBtns);
