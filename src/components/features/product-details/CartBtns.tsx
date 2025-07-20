26e3f58763771fe228c95566efb37fd49ea4a16d
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-20 09:48:26 +0300

    move handleCart function from 'buy now' button to 'add to cart' buttun

--- src/components/features/product-details/CartBtns.tsx
+++ src/components/features/product-details/CartBtns.tsx
@@ -5,13 +5,13 @@ const CartBtns = ({ productId }: { productId: string }) => {
   const { handleCart } = useCartData();
   return (
     <div className="flex items-center gap-3 w-full pb-5 border-b border-soft-gray">
-      <button
-        className="flex-1 border border-primary text-primary font-medium rounded-none px-3 h-auto py-1 text-[15px] hover:bg-primary hover:text-neutral duration-200 cursor-pointer"
-        onClick={() => handleCart?.(productId)}
-      >
+      <button className="flex-1 border border-primary text-primary font-medium rounded-none px-3 h-auto py-1 text-[15px] hover:bg-primary hover:text-neutral duration-200 cursor-pointer">
         Buy now
       </button>
-      <button className="flex-1 border border-primary primary-btn rounded-none! px-3! h-auto! py-1 text-[15px] relative overflow-hidden group cursor-pointer">
+      <button
+        className="flex-1 border border-primary primary-btn rounded-none! px-3! h-auto! py-1 text-[15px] relative overflow-hidden group cursor-pointer"
+        onClick={() => handleCart?.(productId)}
+      >
         Add to cart
         <span className="shine-effect group-hover:animate-shine" />
       </button>
