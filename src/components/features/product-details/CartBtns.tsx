6b850577c4729dddd25397ef21e41276bc9e3af9
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-24 15:57:23 +0300

    generate new prop as a function to pass to the buy now button

--- src/components/features/product-details/CartBtns.tsx
+++ src/components/features/product-details/CartBtns.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
-const CartBtns = ({ handleAddToCart }: { handleAddToCart: () => void }) => {
+type Props = {
+  handleAddToCart: () => void;
+  handleBuyNow: () => void;
+};
+
+const CartBtns = ({ handleAddToCart, handleBuyNow }: Props) => {
   return (
     <div className="flex items-center gap-3 w-full pb-5 border-b border-soft-gray">
-      <button className="flex-1 border border-primary text-primary font-medium rounded-none px-3 h-auto py-1 text-[15px] hover:bg-primary hover:text-neutral duration-200 cursor-pointer">
+      <button
+        className="flex-1 border border-primary text-primary font-medium rounded-none px-3 h-auto py-1 text-[15px] hover:bg-primary hover:text-neutral duration-200 cursor-pointer"
+        onClick={handleBuyNow}
+      >
         اشترِ الآن
       </button>
       <button
