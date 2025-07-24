0cf3360e4d14914e3be75073442f97d2e937203e
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-24 11:58:06 +0300

    render About component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,11 @@
-const About = () => {
-  return <div className="flex-1">About</div>;
+import About from "@/components/features/about";
+
+const AboutPage = () => {
+  return (
+    <div className="flex-1">
+      <About />
+    </div>
+  );
 };
 
-export default About;
+export default AboutPage;
