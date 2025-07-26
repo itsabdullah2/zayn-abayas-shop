5b873c90ac09bbd18cbe643ced24817590d94c86
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-26 09:36:24 +0300

    reorder the position of the UI

--- src/components/features/checkout/index.tsx
+++ src/components/features/checkout/index.tsx
@@ -4,8 +4,8 @@ import OrderSummary from "./OrderSummary";
 const Checkout = () => {
   return (
     <div className="section-container flex flex-col xl:flex-row items-start gap-5">
-      <CheckoutForm />
       <OrderSummary />
+      <CheckoutForm />
     </div>
   );
 };
