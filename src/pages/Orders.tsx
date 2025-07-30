20a0b876e40e1b30b3a37c7c084a87ed9617b1d8
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-30 17:56:26 +0300

    render the Orders UI

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,5 +1,13 @@
+import { lazy } from "react";
+
+const Orders = lazy(() => import("@/components/features/orders"));
+
 const OrdersPage = () => {
-  return <div className="flex-1 bg-light-gray">OrdersPage</div>;
+  return (
+    <div className="flex-1 bg-light-gray">
+      <Orders />
+    </div>
+  );
 };
 
 export default OrdersPage;
