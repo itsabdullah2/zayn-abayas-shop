0be21634c7bb9b5aeef79c5700b63546018991a6
itsabdullah2 <abdullahosman3020@gmail.com>
2025-07-31 16:56:18 +0300

    render the UI of the orders table

--- src/components/features/orders/OrdersTable.tsx
+++ src/components/features/orders/OrdersTable.tsx
@@ -4,16 +4,19 @@ import TableRow from "./TableRow";
 
 const OrdersTable = ({ orders }: { orders: FullOrder[] }) => {
   return (
-    <table className="w-full">
-      <thead className="flex py-3">
-        <th className="flex-1 self-end text-center">أسم المنتج</th>
-        <th className="flex-1 self-end text-center">رقم المعرف</th>
-        <th className="flex-1 self-end text-center">التاريخ</th>
-        <th className="flex-1 self-end text-center">الكمية</th>
-        <th className="flex-1 self-end text-center">السعر</th>
-        <th className="flex-1 self-end text-center">حالة الطلب</th>
+    <table className="w-full bg-neutral rounded-xl border-soft-gray border min-h-[500px] p-5 flex flex-col">
+      <thead className="border-b border-light-gray">
+        <tr className="flex py-3">
+          <th className="flex-1 self-end text-center">أسم المنتج</th>
+          <th className="flex-1 self-end text-center">رقم الطلب</th>
+          <th className="flex-1 self-end text-center">التاريخ</th>
+          <th className="flex-1 self-end text-center">الكمية</th>
+          <th className="flex-1 self-end text-center">السعر</th>
+          <th className="flex-1 self-end text-center">حالة الطلب</th>
+          <th className="flex-1 self-end text-center">خيارات</th>
+        </tr>
       </thead>
-      <tbody className="flex">
+      <tbody className="flex flex-col">
         {orders.map((order) => (
           <TableRow key={order.id} order={order} />
         ))}
