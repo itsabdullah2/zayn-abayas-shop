59b6bd9622a73148ca5b3d04768bb98ec9554b5f
itsabdullah2 <abdullahosman3020@gmail.com>
2025-08-02 20:52:33 +0300

    render the UI of the Dropdown actions

--- src/components/features/orders/DropdownActions.tsx
+++ src/components/features/orders/DropdownActions.tsx
@@ -1,5 +1,33 @@
-const DropdownActions = () => {
-  return <div className="absolute top-0 left-0">Actions</div>;
+import React from "react";
+
+type Props = {
+  openOrderTrackingPopup: () => void;
 };
 
-export default DropdownActions;
+const DropdownActions = ({ openOrderTrackingPopup }: Props) => {
+  return (
+    <div className="absolute top-8 left-1/2 rounded-xl bg-neutral w-38 z-50 py-2 px-2 shadow-lg">
+      <ul className="flex flex-col gap-1">
+        <li className="text-sm px-2 py-2 hover:bg-light-gray text-text hover:text-primary hover:pr-1 rounded-md w-full duration-150 text-right">
+          <button className="text-sm cursor-pointer">إلغاء الطلب</button>
+        </li>
+        <li className="text-sm px-2 py-2 hover:bg-light-gray text-text hover:text-primary hover:pr-1 rounded-md w-full duration-150 text-right">
+          <button
+            className="text-sm cursor-pointer"
+            onClick={openOrderTrackingPopup}
+          >
+            تتبع الطلب
+          </button>
+        </li>
+        <li className="text-sm px-2 py-2 hover:bg-light-gray text-text hover:text-primary hover:pr-1 rounded-md w-full duration-150 text-right">
+          <button className="text-sm cursor-pointer">إرجاع الطلب</button>
+        </li>
+        <li className="text-sm px-2 py-2 hover:bg-light-gray text-text hover:text-primary hover:pr-1 rounded-md w-full duration-150 text-right">
+          <button className="text-sm cursor-pointer">طباعة</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default React.memo(DropdownActions);
