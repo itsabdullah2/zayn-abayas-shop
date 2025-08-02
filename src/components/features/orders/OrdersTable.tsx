5c85f756cc07bc3636a2588869ba0deedb6fb82a
itsabdullah2 <abdullahosman3020@gmail.com>
2025-08-02 16:35:54 +0300

    pass the values to the props and create a function to generate order number

--- src/components/features/orders/OrdersTable.tsx
+++ src/components/features/orders/OrdersTable.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import type { FullOrder } from "@/supabase/types";
 import TableRow from "./TableRow";
 
 const OrdersTable = ({ orders }: { orders: FullOrder[] }) => {
+  const [dropdownActions, setDropdownActions] = useState<string | null>(null);
+
+  const generateOrderNumber = useMemo(() => {
+    const random = Math.floor(100000000 + Math.random() * 900000000); // 9-digits
+    return `#${random}`;
+  }, []);
+
+  const handleDropdownActions = (id: string) => {
+    setDropdownActions((prev) => (prev === id ? null : id));
+  };
+
   return (
-    <table className="w-full bg-neutral rounded-xl border-soft-gray border min-h-[500px] p-5 flex flex-col">
+    <table className="w-full bg-neutral rounded-xl border-soft-gray border min-h-[450px] p-5 flex flex-col">
       <thead className="border-b border-light-gray">
         <tr className="flex py-3">
           <th className="flex-1 self-end text-center">أسم المنتج</th>
@@ -18,7 +29,13 @@ const OrdersTable = ({ orders }: { orders: FullOrder[] }) => {
       </thead>
       <tbody className="flex flex-col">
         {orders.map((order) => (
-          <TableRow key={order.id} order={order} />
+          <TableRow
+            key={order.id}
+            order={order}
+            dropdownActions={dropdownActions}
+            handleDropdownActions={handleDropdownActions}
+            generateOrderNumber={generateOrderNumber}
+          />
         ))}
       </tbody>
     </table>
