d0e50b45e99b04f5be9844e3ca7c8b758197704d
itsabdullah2 <abdullahosman3020@gmail.com>
2025-08-02 20:52:35 +0300

    initial commit

---
+++ src/components/features/orders/OrderTrackingPopup.tsx
@@ -0,0 +1,9 @@
+const OrderTrackingPopup = () => {
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      OrderTrackingPopup
+    </div>
+  );
+};
+
+export default OrderTrackingPopup;
