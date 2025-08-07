a4bd73db48de8a370e6612a065c7a611e3693b2d
itsabdullah2 <abdullahosman3020@gmail.com>
2025-08-07 15:11:37 +0300

    render default UI for testing

--- src/components/features/orders/OrderTrackingPopup.tsx
+++ src/components/features/orders/OrderTrackingPopup.tsx
@@ -1,8 +1,25 @@
+import { OrdersContext } from "@/context/OrdersContext";
+import { useContextSelector } from "use-context-selector";
+
 const OrderTrackingPopup = () => {
+  const closeTrackingPopup = useContextSelector(
+    OrdersContext,
+    (ctx) => ctx?.closeTrackingPopup
+  );
+
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      OrderTrackingPopup
-    </div>
+    <>
+      <div className="absolute top-0 left-0 w-full h-full bg-black/60 z-40" />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 text-white">
+        OrderTrackingPopup
+        <button
+          className="w-5 h-5 border border-soft-gray flex-center rounded-md cursor-pointer"
+          onClick={closeTrackingPopup}
+        >
+          X
+        </button>
+      </div>
+    </>
   );
 };
 
