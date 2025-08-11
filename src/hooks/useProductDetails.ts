dfe44257f548fe213cd20d1a3e75fd56dbd8f3b5
itsabdullah2 <abdullahosman3020@gmail.com>
2025-08-11 13:42:05 +0300

    fetch reviews data

--- src/hooks/useProductDetails.ts
+++ src/hooks/useProductDetails.ts
@@ -1,5 +1,15 @@
-import { getColors, getProducts, getSizes, getVariants } from "@/supabase";
-import type { ColorsAndSizesType, VariantsTableType } from "@/supabase/types";
+import {
+  getColors,
+  getProducts,
+  getReviews,
+  getSizes,
+  getVariants,
+} from "@/supabase";
+import type {
+  ColorsAndSizesType,
+  ReviewsTableType,
+  VariantsTableType,
+} from "@/supabase/types";
 import type { ProductType } from "@/types";
 import { useEffect, useState } from "react";
 
@@ -7,6 +17,7 @@ const useProductDetails = (id?: string) => {
   const [product, setProduct] = useState<ProductType | null>(null);
   const [variants, setVariants] = useState<VariantsTableType[]>([]);
   const [colors, setColors] = useState<ColorsAndSizesType[]>([]);
+  const [reviews, setReviews] = useState<ReviewsTableType[]>([]);
   const [sizes, setSizes] = useState<ColorsAndSizesType[]>([]);
   const [loading, setLoading] = useState(false);
 
@@ -14,16 +25,18 @@ const useProductDetails = (id?: string) => {
     const fetchProduct = async () => {
       try {
         setLoading(true);
-        const [productRes, variantsRes, colorsRes, sizesRes] =
+        const [productRes, variantsRes, reviewsRes, colorsRes, sizesRes] =
           await Promise.all([
             getProducts({ eqCol: "id", eqVal: id }),
             getVariants({ productId: id }),
+            getReviews(id ?? ""),
             getColors(),
             getSizes(),
           ]);
 
         setProduct(productRes[0]);
         setVariants(variantsRes);
+        setReviews(reviewsRes);
         setColors(colorsRes);
         setSizes(sizesRes);
       } catch (err) {
@@ -43,6 +56,7 @@ const useProductDetails = (id?: string) => {
     colors,
     sizes,
     loading,
+    reviews,
   };
 };
 
