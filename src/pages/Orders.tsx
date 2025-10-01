f7793b904035de0c6a7dff1d5d630e188cf6503d
itsabdullah2 <abdullahosman3020@gmail.com>
2025-10-01 18:40:19 +0300

    move the OrderTrackingPopup component to OrdersTable component

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,18 +1,10 @@
 import { lazy } from "react";
-import { useContextSelector } from "use-context-selector";
-import { OrdersContext } from "@/context/OrdersContext";
-import OrderTrackingPopup from "@/components/features/orders/OrderTrackingPopup";
 
 const Orders = lazy(() => import("@/components/features/orders"));
 
 const OrdersPage = () => {
-  const isTrackingPopup = useContextSelector(
-    OrdersContext,
-    (ctx) => ctx?.isTrackingPopup
-  );
   return (
     <div className="flex-1 bg-light-gray">
-      {isTrackingPopup && <OrderTrackingPopup />}
       <Orders />
     </div>
   );
