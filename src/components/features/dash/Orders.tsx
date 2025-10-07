4fab9645ae76337bf72884dbf048dd96ad8b9a85
itsabdullah2 <abdullahosman3020@gmail.com>
2025-10-07 18:43:15 +0300

    render dashboard navbar

--- src/components/features/dash/Orders.tsx
+++ src/components/features/dash/Orders.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Table from "./Table";
 import type { FullOrder } from "@/supabase/types";
 import { getAllOrders } from "@/supabase/db/orders";
+import DashboardNavbar from "./DashboardNavbar";
 
 const Orders = () => {
   const [orders, setOrders] = useState<FullOrder[]>([]);
@@ -25,10 +26,13 @@ const Orders = () => {
   }, []);
 
   return (
-    <section className="section-container">
-      <h2 className="text-xl font-medium text-primary">الطبات</h2>
-      <Table orders={orders} loading={loading} />
-    </section>
+    <>
+      <DashboardNavbar />
+      <section className="section-container">
+        <h2 className="text-xl font-medium text-primary">الطبات</h2>
+        <Table orders={orders} loading={loading} />
+      </section>
+    </>
   );
 };
 
