b886fe1dd742fb9bfc82c6414bc8f60b1b004cb9
itsabdullah2 <abdullahosman3020@gmail.com>
2025-10-07 18:43:05 +0300

    render dashboard navbar

--- src/components/features/dash/AdminDashboard.tsx
+++ src/components/features/dash/AdminDashboard.tsx
@@ -1,21 +1,29 @@
+import DashboardNavbar from "./DashboardNavbar";
+
 const AdminDashboard = () => {
   return (
-    <section className="section-container">
-      <h2 className="text-xl font-medium text-primary">لوحة التحكم المسؤول</h2>
-      <div className="flex flex-col gap-8 mt-10">
-        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-          <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
-          <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
+    <>
+      <DashboardNavbar />
+      <section className="section-container">
+        <h2 className="text-xl font-medium text-primary">
+          لوحة التحكم المسؤول
+        </h2>
+        <div className="flex flex-col gap-8 mt-10">
+          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
+            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
+            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
+            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
+          </div>
+          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
+            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
+            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
+          </div>
           <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
         </div>
-        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-          <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
-          <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
-          <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
-        </div>
-        <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
