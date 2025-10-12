32438a6134796e41db29f0cf513d32f5540d69ff
itsabdullah2 <abdullahosman3020@gmail.com>
2025-10-12 15:15:32 +0300

    render the initial content

--- src/components/features/dash/AdminDashboard.tsx
+++ src/components/features/dash/AdminDashboard.tsx
@@ -1,25 +1,16 @@
+import DashboardAnalytics from "./DashboardAnalytics";
 import DashboardNavbar from "./DashboardNavbar";
+import DashboardUpperBoxes from "./DashboardUpperBoxes";
 
 const AdminDashboard = () => {
   return (
     <>
       <DashboardNavbar />
       <section className="section-container">
-        <h2 className="text-xl font-medium text-primary">
-          لوحة التحكم المسؤول
-        </h2>
+        <h2 className="text-xl font-medium text-primary">لوحة تحكم المسؤول</h2>
         <div className="flex flex-col gap-8 mt-10">
-          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
-            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
-            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
-            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
-          </div>
-          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
-            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
-            <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
-          </div>
+          <DashboardUpperBoxes />
+          <DashboardAnalytics />
           <div className="border border-gray-400 py-3 px-5 rounded-lg min-h-20" />
         </div>
       </section>
