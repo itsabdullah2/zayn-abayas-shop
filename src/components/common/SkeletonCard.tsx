f42fa1a803dd3eb0c209ad36b207f7a27ad5c6f0
itsabdullah2 <abdullahosman3020@gmail.com>
2025-11-02 22:25:46 +0200

    modify tailwind classes

--- src/components/common/SkeletonCard.tsx
+++ src/components/common/SkeletonCard.tsx
@@ -3,10 +3,10 @@ import { Skeleton } from "@/components/ui/skeleton";
 export function SkeletonCard() {
   return (
     <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[225px] rounded-xl bg-light-gray" />
-      <div className="space-y-2 flex items-center gap-5">
-        <Skeleton className="flex-1 h-4 bg-light-gray" />
-        <Skeleton className="flex-1 h-4 bg-light-gray" />
+      <Skeleton className="h-[225px] rounded-xl bg-gray-300" />
+      <div className="flex items-center gap-5">
+        <Skeleton className="flex-1 h-4 bg-gray-300" />
+        <Skeleton className="flex-1 h-4 bg-gray-300" />
       </div>
     </div>
   );
