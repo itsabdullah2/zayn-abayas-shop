b705e8a45c8d000d00115a6c4f367c7825a8ea7f
itsabdullah2 <abdullahosman3020@gmail.com>
2025-11-03 20:44:24 +0200

    change tailwind classes of the boxes

--- src/components/features/dash/dashboard/DashboardUpperBoxes.tsx
+++ src/components/features/dash/dashboard/DashboardUpperBoxes.tsx
@@ -19,8 +19,8 @@ export default function DashboardUpperBoxes() {
 
   return (
     <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      <div className="border border-gray-400 py-2 px-3 rounded-lg min-h-20 flex items-center gap-3 bg-light-gray hover:shadow-xl duration-200">
-        <div className="bg-gray-300 w-11 h-11 flex-center rounded-full">
+      <div className="py-2 px-3 rounded-lg min-h-20 flex items-center gap-3 bg-neutral/40 hover:shadow-xl duration-200">
+        <div className="bg-gray-300/40 w-11 h-11 flex-center rounded-full">
           <FaBox size={23} />
         </div>
         <div className="flex flex-col gap-2">
@@ -28,8 +28,8 @@ export default function DashboardUpperBoxes() {
           <span className="text-[15px] text-gray">{orders?.length ?? 0}</span>
         </div>
       </div>
-      <div className="border border-gray-400 py-2 px-3 rounded-lg min-h-20 flex items-center gap-3 bg-light-gray hover:shadow-xl duration-200">
-        <div className="bg-gray-300 w-11 h-11 flex-center rounded-full">
+      <div className="py-2 px-3 rounded-lg min-h-20 flex items-center gap-3 bg-neutral/40 hover:shadow-xl duration-200">
+        <div className="bg-gray-300/40 w-11 h-11 flex-center rounded-full">
           <BsClipboard2CheckFill size={23} />
         </div>
         <div className="flex flex-col gap-2">
@@ -37,8 +37,8 @@ export default function DashboardUpperBoxes() {
           <span className="text-[15px] text-gray">{completedCount}</span>
         </div>
       </div>
-      <div className="border border-gray-400 py-2 px-3 rounded-lg min-h-20 flex items-center gap-3 bg-light-gray hover:shadow-xl duration-200">
-        <div className="bg-gray-300 w-11 h-11 flex-center rounded-full">
+      <div className="py-2 px-3 rounded-lg min-h-20 flex items-center gap-3 bg-neutral/40 hover:shadow-xl duration-200">
+        <div className="bg-gray-300/40 w-11 h-11 flex-center rounded-full">
           <BsClipboard2XFill size={23} />
         </div>
         <div className="flex flex-col gap-2">
@@ -46,8 +46,8 @@ export default function DashboardUpperBoxes() {
           <span className="text-[15px] text-gray">{cancelledCount}</span>
         </div>
       </div>
-      <div className="border border-gray-400 py-2 px-3 rounded-lg min-h-20 flex items-center gap-3 bg-light-gray hover:shadow-xl duration-200">
-        <div className="bg-gray-300 w-11 h-11 flex-center rounded-full">
+      <div className="py-2 px-3 rounded-lg min-h-20 flex items-center gap-3 bg-neutral/40 hover:shadow-xl duration-200">
+        <div className="bg-gray-300/40 w-11 h-11 flex-center rounded-full">
           <FaAward size={23} />
         </div>
         <div className="flex flex-col gap-2">
