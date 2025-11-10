8d16e39f51bb938548118eb9573bfc2dd47ddc9f
itsabdullah2 <abdullahosman3020@gmail.com>
2025-11-10 16:49:41 +0200

    add the text of the error404 page

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+
 const Error404 = () => {
-  return <div className="flex-1">Error404</div>;
+  return (
+    <div className="flex-1 flex-center">
+      <div className="flex flex-col items-center gap-3">
+        <h1 className="text-2xl font-medium text-primary">
+          هذه الصفخة ليست متاحة الأن
+        </h1>
+
+        <Link
+          to="/"
+          className="py-2 px-5 relative group overflow-hidden primary-btn rounded-none! w-fit mt-2 shadow-lg"
+        >
+          <span className="relative z-10 text-[15px]">الذهاب الى الرئيسية</span>
+          {/* Scanner Shine */}
+          <span className="shine-effect group-hover:animate-shine" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Error404;
