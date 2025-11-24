226a2f81f5e0fda44fafc833042565cb2c5932e4
itsabdullah2 <abdullahosman3020@gmail.com>
2025-11-24 19:24:56 +0200

    render the actual popup of adding a new product

--- src/components/features/dash/products/AddNewProduct.tsx
+++ src/components/features/dash/products/AddNewProduct.tsx
@@ -1,5 +1,6 @@
 import { memo } from "react";
-import { IoIosClose } from "react-icons/io";
+import PopupField from "./PopupField";
+import uploadImage from "@/assets/upload.png";
 
 type Props = {
   isNewProduct: boolean;
@@ -7,17 +8,95 @@ type Props = {
 };
 
 const AddNewProduct = ({ isNewProduct, setProductChange }: Props) => {
+  const handleFieldChange = () => {};
+
   return isNewProduct ? (
     <>
       <div className="fixed top-0 left-0 w-full h-full bg-black/60 z-100" />
-      <div className="w-[95vw] sm:w-[500px] bg-white rounded-xl py-4 px-5 fixed top-1/2 left-1/2 z-200 -translate-x-1/2 -translate-y-1/2 text-center">
-        This Functionality is Coming Soon!
-        <button
-          className="absolute -top-7 right-0 text-light-gray border-light-gray border rounded-full w-5 h-5 flex items-center justify-center text-2xl cursor-pointer"
-          onClick={() => setProductChange(false)}
-        >
-          <IoIosClose size={20} className="" />
-        </button>
+      <div className="w-[95vw] sm:w-[500px] bg-white rounded-xl py-4 px-5 fixed top-1/2 left-1/2 z-200 -translate-x-1/2 -translate-y-1/2">
+        <h2 className="font-medium text-primary text-lg">
+          قم بإضافة منتج جديد
+        </h2>
+        {/* Popup Fields */}
+        <form id="adding_form" className="flex flex-col gap-2 mt-4">
+          <PopupField
+            label="تحميل صورة المنتج"
+            labelIcon={uploadImage}
+            // imgName={
+            //   editingData.productImg instanceof File
+            //     ? editingData.productImg.name
+            //     : editingData.productImg
+            // }
+            type="file"
+            name="productImg"
+            inputVal={""}
+            onValChange={handleFieldChange}
+            inputClasses="hidden"
+            isFileInput
+          />
+          <PopupField
+            label="اسم المنتج"
+            type="text"
+            name="productName"
+            placeholder="أدخل اسم المنتج"
+            inputVal={""}
+            onValChange={handleFieldChange}
+          />
+          <PopupField
+            label="سعر المنتج"
+            type="number"
+            name="productPrice"
+            placeholder="أدخل سعر المنتج"
+            inputVal={""}
+            onValChange={handleFieldChange}
+          />
+          <PopupField
+            label="وصف المنتج"
+            type="text"
+            name="productDesc"
+            placeholder="وصف المنتج"
+            inputVal={""}
+            onValChange={handleFieldChange}
+            isTextarea
+          />
+
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="productCategory"
+              className="text-sm font-medium text-dark-gray w-fit"
+            >
+              فئة المنتج
+            </label>
+            <select
+              name="productCategory"
+              id="productCategory"
+              className="w-1/2 border border-primary rounded-md focus:outline-none"
+            >
+              <option disabled>قم باختيار فئة المنتج</option>
+              <option value="luxury">فاخرة</option>
+              <option value="modern">عصرية</option>
+              <option value="classic">كلاسيكية</option>
+            </select>
+          </div>
+        </form>
+
+        <div className="items-center flex justify-between mt-7">
+          <button
+            type="submit"
+            form="adding_form"
+            className={`cursor-pointer bg-accentA text-neutral rounded-lg px-10 py-1 hover:bg-accentA duration-150`}
+            // disabled={updateProductMutation.isPending}
+          >
+            {/* {updateProductMutation.isPending ? "جاري التحديث..." : "تأكيد"} */}
+            تأكيد
+          </button>
+          <button
+            className="cursor-pointer border border-accentA rounded-lg px-10 py-1 hover:bg-accentA duration-150 hover:text-white"
+            onClick={() => setProductChange(false)}
+          >
+            الغاء
+          </button>
+        </div>
       </div>
     </>
   ) : null;
