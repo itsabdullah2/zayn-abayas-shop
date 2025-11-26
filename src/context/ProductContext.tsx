87711f4ec9194ce9447a0110db0a5cb4344c532e
itsabdullah2 <abdullahosman3020@gmail.com>
2025-11-26 10:39:49 +0200

    context for admin prodvu

---
+++ src/context/ProductContext.tsx
@@ -0,0 +1,68 @@
+import { createContext } from "use-context-selector";
+import { useState } from "react";
+
+export type TProductData = {
+  productName: string;
+  productPrice: number;
+  productDesc: string;
+  productImg: string | File;
+};
+
+type OrderContextType = {
+  newProductData: TProductData;
+  handleFieldChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+};
+
+export const ProductContext = createContext<OrderContextType | null>(null);
+
+const INITIAL_STATE: TProductData = {
+  productName: "",
+  productPrice: 0,
+  productDesc: "",
+  productImg: "",
+};
+
+export const ProductProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [newProductData, setNewProductData] =
+    useState<TProductData>(INITIAL_STATE);
+
+  const handleFieldChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+
+    if (e.target instanceof HTMLInputElement && e.target.type === "file") {
+      const file = e.target.files?.[0];
+
+      if (!file) return;
+
+      setNewProductData((prev) => ({
+        ...prev,
+        [name]: file,
+      }));
+
+      return;
+    }
+
+    setNewProductData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const values: OrderContextType = {
+    newProductData,
+    // Functions
+    handleFieldChange,
+  };
+
+  return (
+    <ProductContext.Provider value={values}>{children}</ProductContext.Provider>
+  );
+};
