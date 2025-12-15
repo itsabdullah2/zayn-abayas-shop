de433e1e5aa392b9653403596754423457d10294
itsabdullah2 <abdullahosman3020@gmail.com>
2025-12-15 17:34:12 +0200

    custom hook to handle closing on outside & on escape

---
+++ src/hooks/useCloseOnOutsideOrEscape.ts
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+
+type Props = {
+  ref: React.RefObject<HTMLDivElement | null>;
+  onClose: () => void;
+};
+
+export const useCloseOnOutsideOrEscape = ({ ref, onClose }: Props) => {
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        onClose();
+      }
+    };
+
+    const handleEscapeKey = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("keydown", handleEscapeKey);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, [ref, onClose]);
+};
