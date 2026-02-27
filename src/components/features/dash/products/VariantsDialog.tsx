2bc14bd8359f5f48df3bf6f6755cf93ab643f36f
itsabdullah2 <abdullahosman3020@gmail.com>
2026-02-27 23:54:21 +0200

    initial commit for variants dialog

---
+++ src/components/features/dash/products/VariantsDialog.tsx
@@ -0,0 +1,41 @@
+import { ProductContext } from "@/context/ProductContext";
+import { useCloseOnOutsideOrEscape } from "@/hooks/useCloseOnOutsideOrEscape";
+import { useGetVariantByProductId } from "@/hooks/useVariants";
+import React from "react";
+import { useContextSelector } from "use-context-selector";
+
+const VariantsDialog = ({
+  productId,
+  ref,
+}: {
+  productId: string;
+  ref: React.RefObject<HTMLDivElement | null>;
+}) => {
+  const { data: variants = [], isLoading } =
+    useGetVariantByProductId(productId);
+
+  const handleTargetDialog = useContextSelector(
+    ProductContext,
+    (ctx) => ctx?.handleTargetDialog,
+  )!;
+
+  useCloseOnOutsideOrEscape({
+    ref: ref,
+    onClose: () => handleTargetDialog(null),
+  });
+
+  return (
+    <div
+      ref={ref}
+      className={`w-full bg-light-gray py-1 px-2 rounded-lg absolute left-0 top-[calc(100%+10px)] shadow-md z-10 min-h-20`}
+    >
+      <ul className="flex flex-col gap-2">
+        {isLoading
+          ? "Loading..."
+          : variants.map((v) => <li key={v.id}>{v.price}</li>)}
+      </ul>
+    </div>
+  );
+};
+
+export default React.memo(VariantsDialog);
