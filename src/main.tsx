c8c979088441f56145628b72428dd3680b95db92
itsabdullah2 <abdullahosman3020@gmail.com>
2026-03-21 08:04:49 +0200

    import queryClient from outside for adding some additional customziation

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,9 @@ import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
 import { StripeProvider } from "./context/StripeContext";
 import { CheckoutProvider } from "./context/CheckoutContext";
+import { queryClient } from "./";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
+import { QueryClientProvider } from "@tanstack/react-query";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -30,5 +29,5 @@ createRoot(document.getElementById("root")!).render(
         </StripeProvider>
       </BrowserRouter>
     </QueryClientProvider>
-  </StrictMode>
+  </StrictMode>,
 );
