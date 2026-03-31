2ff0153b2728452822152c3a9601fd94a61ab44e
itsabdullah2 <abdullahosman3020@gmail.com>
2026-03-31 20:59:57 +0200

    disable the button when any of the required fields are empty

--- src/components/features/checkout/SubmitButton.tsx
+++ src/components/features/checkout/SubmitButton.tsx
@@ -4,20 +4,22 @@ interface SubmitButtonProps {
   loading: boolean;
   total: number;
   stripeAvailable: boolean;
+  isFormValid: boolean;
 }
 
 const SubmitButton = ({
   loading,
   total,
   stripeAvailable,
+  isFormValid,
 }: SubmitButtonProps) => (
   <CustomButton
     type="submit"
-    isDisabled={!stripeAvailable || loading}
-    className={`w-full py-3 rounded-lg text-white font-semibold cursor-pointer ${
-      loading || !stripeAvailable
+    isDisabled={!stripeAvailable || loading || isFormValid}
+    className={`w-full py-3 rounded-lg text-white font-semibold ${
+      loading || !stripeAvailable || isFormValid
         ? "bg-soft-gray cursor-not-allowed"
-        : "bg-primary relative overflow-hidden group"
+        : "bg-primary relative overflow-hidden group cursor-pointer"
     }`}
     btnText={loading ? "جارٍ المعالجة..." : `ادفع ${total} ج.م`}
   />
